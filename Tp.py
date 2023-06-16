d2268156fdd13cbcd46aaddcbbbc84941ba7cbd7
catafrulo <catafrulo@users.noreply.github.com>
2023-06-16 19:21:30 -0300

    V1

---
+++ Tp.py
@@ -0,0 +1,43 @@
+menuFunciones = [
+    AltaEstudiante(), BajaEstudiante(), ModificacionEstudiante(), ListadoEstudiantes(), CargaNotaEstudiante(),
+    ListaEstudiantesReprobados(), ListaEstudiantesAprobados(), ListadoEstudiantes(), SalirPrograma()   
+]
+menuOpciones = [
+    "Alta Estudiante", 
+    "Baja Estudiante", 
+    "Modificacion Estudiante", 
+    "Listado Estudiantes", 
+    "Carga de Nota Estudiante", 
+    "Listado de Estudiantes Reprobados", #Nro. Legajo, Nombre y Nota
+    "Listado de Estudiantes Aprobados", #Nro. Legajo, Nombre y Nota
+    "Listado de Mejores Estudiantes", #Nro. Legajo, Nombre y Nota
+    "Salir del Programa"
+] 
+def Main():
+
+def AltaEstudiante():
+    
+    
+def BajaEstudiante():
+    
+    
+def ModificacionEstudiante():
+    
+    
+def ListadoEstudiantes():
+    
+    
+def CargaNotaEstudiante():
+    
+    
+def ListaEstudiantesReprobados():
+    
+    
+def ListaEstudiantesAprobados():
+    
+     
+def ListadoMejoresEstudiantes():
+    
+    
+def SalirPrograma():
+    
