b1a337c50f36382f700db497a336bdc19df075b9
AugstR <133713443+AugstR@users.noreply.github.com>
2023-06-29 23:10:40 -0300

    alta estd

--- Tp.py
+++ Tp.py
@@ -1,25 +1,42 @@
-def AltaEstudiante():
-    print ("a")
+legajosLista=[]
+nombresLista=[]
 
-def BajaEstudiante():
+
+def AltaEstudiante():  #Se ingresa legajo y nombre
+    print ("Alta Estudiante")
+    legajo=int(input("Ingrese legajo => "))
+    i=0
+    maximoi=len(legajosLista)
+    encontrado=False
+    while i < legajosLista and encontrado==False:
+        if legajo==legajosLista [i]:
+            encontrado=True
+        else:
+            i=+1
+        
+
+
+    
+def BajaEstudiante(): #se ingresa legajo y se elimina todas las listas de estudiantes
     print ("a")
     
-def ModificacionEstudiante():
+def ModificacionEstudiante(): #se ingresa legajo, se muestra el nombre actual, y se permite cambiar el nombre
     print ("a")
     
-def ListadoEstudiantes():
+def ListadoEstudiantes():#ordenado por nro. de Legajo (por metodo de selección)
     print ("a")
     
-def CargaNotaEstudiante():
+def CargaNotaEstudiante():  #Se ingresa legajo y nota (entre 1 y 10,siendo las notas nro. enteros)
+                            #En caso que el legajo no haya sido cargado, se debe mostrar la leyenda “Legajo inexistente”. La carga finaliza con legajo igual a -1. 
     print ("a")
     
-def ListaEstudiantesReprobados():
+def ListaEstudiantesReprobados():#(nro. de Legajo ,  nombre y nota) 
     print ("a")
     
-def ListaEstudiantesAprobados():
+def ListaEstudiantesAprobados():#(nro. de Legajo ,  nombre y nota) 
     print ("a")
      
-def ListadoMejoresEstudiantes():
+def ListadoMejoresEstudiantes(): #estudiantes (nro. de Legajo ,  nombre y nota) de aquellos  que tengan nota más alta 
     print ("a")
     
 
