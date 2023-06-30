456cab6498aba745c664b2f8a751df87e2e19da2
geb2701 <86382687+geb2701@users.noreply.github.com>
2023-06-30 01:05:02 -0300

    alta notas

--- Tp.py
+++ Tp.py
@@ -1,6 +1,6 @@
 legajosLista=[3,2,1]
 nombresLista=["Juan","Pedro","Maxi"]
-notas=[0,0,0]
+notas=[1,5,8]
 
 def EncontrarLegajo(legajo, legajoCreado): #legajoCreado es un boleado que determina si existe o no el legajo
                                             #True el legajo ya esta creado
@@ -26,7 +26,7 @@ def EncontrarLegajo(legajo, legajoCreado): #legajoCreado es un boleado que deter
             i=0
         else:
             legajoBusqueda = True
-    return [legajo, i]
+    return [legajo, i] #retornamos el numero del legajo y su posicion en el array
 
 def OrdenarListas():
     n=len(legajosLista)
@@ -128,7 +128,7 @@ def BajaEstudiante(): #se ingresa legajo y se elimina todas las listas de estudi
     else:
         print("Operacion Cancelada")
 
-    nuevamente = str(input("Pulse 1 si quiere Realizar una Nueva Operación, de lo contario Enter para volver al menu"))
+    nuevamente = str(input("Pulse 1 si quiere Realizar una Nueva Operación, de lo contario Enter para volver al Menu =>"))
     if nuevamente == "1": #no usamos cambiante a lower o upper
         AltaEstudiante()
 
@@ -136,59 +136,33 @@ def ModificacionEstudiante(): #se ingresa legajo, se muestra el nombre actual, y
     print ("a")
     
 def ListadoEstudiantes():#ordenado por nro. de Legajo (por metodo de selección)
-    n=len(legajosLista)
-    desordenado=True
-    while desordenado:
-        desordenado=False
-        for j in range(1,n):
-            if (legajosLista[j-1] > legajosLista[j]):
-                
-                aux=legajosLista[j-1]
-                legajosLista[j-1]=legajosLista[j]
-                legajosLista[j]=aux
-
-                aux=nombresLista[j-1]
-                nombresLista[j-1]=nombresLista[j]
-                nombresLista[j]=aux
-
-                aux=notas[j-1]
-                notas[j-1]=notas[j]
-                notas[j]=aux
-                
-                desordenado=True
-        n-=1
+    OrdenarListas()
 
     for i in range (len(legajosLista)):
         print (str(legajosLista[i]) + ")", nombresLista[i])
 
     input("Pulse para Continuar => ")
 
-    
 def CargaNotaEstudiante():  #Se ingresa legajo y nota (entre 1 y 10,siendo las notas nro. enteros)
                             #En caso que el legajo no haya sido cargado, se debe mostrar la leyenda “Legajo inexistente”. La carga finaliza con legajo igual a -1. 
     print ("Alta Notas")
     print("Ingrese 0 o -1 para regresar")
     legajo=int(input("Ingrese el Legajo del Alumno => "))
-    i=0
+    
+    encontrarLegajo = EncontrarLegajo(legajo, False)
+    legajo = encontrarLegajo[0]
+    indice = encontrarLegajo[1]
+    
+    if (notas[indice]!= 0):
+        print("Este Alumno ya tiene una Nota Cargada")
+    else:
+        nota = int(input("Ingrese la Nota del Alumno"))
+        while 1 > nota or nota > 10: #validamos que la nota este entre 1 y 10
+            print("La Nota debe estar entre 1 y 10")
+            nota = int(input("Ingrese la Nota del Alumno"))
+        
 
-    if legajo == 0 or legajo == -1:
-        return
-    legajoListo = False
-    while legajoListo == False:
-        encontrado=False
-        while i < len(legajosLista) and encontrado==False:
-            if legajo == legajosLista[i]:
-                encontrado=True
-            else:
-                i += 1
-        if encontrado == True:
-            print ("Legajo Existente")
-            print("Ingrese 0 para regresar")
-            legajo=int(input("Ingrese el Legajo del Alumno => "))
-            if legajo == 0 or legajo == -1:
-                return
-        else:
-            legajoListo = True
+
     
 def ListaEstudiantesReprobados():#(nro. de Legajo ,  nombre y nota) 
     print ("a")
